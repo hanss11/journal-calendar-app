ea0c5a13e5c8c3b105ea4d7c4fea18003f4bfdc9
hanss <hanssbcr@gmail.com>
2021-04-16 22:51:37 -0300

    adding style loginscreen

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LoginScreen = () => {
-    return (
-        <div>
-            <h1>Login screen</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <h2 className="auth__title">Login</h2>
 
-export default LoginScreen
+      <div>
+        <form action="">
+          <input
+            className="auth__input"
+            type="text"
+            placeholder="ingrese su email"
+            name="name"
+            autoComplete="off"
+          />
+          <input 
+            className="auth__input"
+            type="password" 
+            placeholder="contraseña" 
+            name="password" 
+        />
+          <button 
+            type="submit" 
+            className="btn btn-primary"
+          >Login</button>
+          
+          <div className="auth__social-networks mt-5">
+            <p>Login with social networks</p>
+
+            <div className="google-btn">
+              <div className="google-icon-wrapper">
+                <img
+                  className="google-icon"
+                  src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                  alt="google button"
+                />
+              </div>
+              <p className="btn-text">
+                <b>Sign in with google</b>
+              </p>
+            </div>
+          </div>
+
+          <Link to="/auth/register">Create new account</Link>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default LoginScreen;
--- src/components/routers/AuthRouter.jsx
+++ src/components/routers/AuthRouter.jsx
@@ -5,11 +5,15 @@ import RegisterScreen from '../auth/RegisterScreen';
 
 const AuthRouter = () => {
     return (
+        <div className="auth__main">
+            <div className="auth__box-container">
        <Switch>
            <Route path={"/auth/login"} component={LoginScreen}/>
            <Route path={"/auth/register"} component={RegisterScreen}/>
            <Redirect to={"/auth/login"}/>
        </Switch>
+            </div>
+        </div>
     )
 }
 
